e39397f3a7c1232cbbe16926918757dc9245b6c0
Kay Johansen <kay-johansen@pluralsight.com>
2013-04-03 09:00:20 -0600

    Removed code so there will be something for people to do for the exercises

--- AcceptanceTestDemo.SpecFlow/RegistrationPriceSteps.cs
+++ AcceptanceTestDemo.SpecFlow/RegistrationPriceSteps.cs
@@ -1,63 +1,39 @@
-﻿using System.Linq;
-using AcceptanceTestDemo.Application;
-using AcceptanceTestDemo.Controllers;
-using AcceptanceTestDemo.Repositories;
-using AcceptanceTestDemo.Utilities;
-using NUnit.Framework;
-using TechTalk.SpecFlow;
+﻿using TechTalk.SpecFlow;
 
 namespace AcceptanceTestDemo.SpecFlow
 {
     [Binding]
     public class RegistrationPriceSteps
     {
-        readonly ConferencesController conferencesController = new ConferencesController();
-        readonly ConferencesRepository conferencesRepository = new ConferencesRepository();
-
-        int NumberOfRegistrations;
-        string ConferenceName;
-        string CouponCode = "";
-        int DaysUntilConference = 10;
-
         [Given(@"I am purchasing (.*) registration for (.*)")]
         [Given(@"I am purchasing (.*) registrations for (.*)")]
         public void GivenIAmPurchasingRegistrationFor(int numberOfRegistrations, string conferenceName)
         {
-            NumberOfRegistrations = numberOfRegistrations;
-            ConferenceName = conferenceName;
+            ScenarioContext.Current.Pending();
         }
         
         [Given(@"I provide coupon code (.*)")]
         public void GivenIProvideCouponCode(string couponCode)
         {
-            CouponCode = couponCode;
+            ScenarioContext.Current.Pending();
         }
         
         [Given(@"there are (.*) days until the conference")]
         public void GivenThereAreDaysUntilTheConference(int daysUntilConference)
         {
-            DaysUntilConference = daysUntilConference;
+            ScenarioContext.Current.Pending();
         }
         
         [Then(@"the total price should be (.*)")]
         public void ThenTheTotalPriceShouldBe(decimal expectedTotalPrice)
         {
-            var result = GetRegistrationPrice();
-            Assert.That(result.TotalPrice, Is.EqualTo(expectedTotalPrice));
+            ScenarioContext.Current.Pending();
         }
 
         [Then(@"the unit price should be (.*)")]
         public void ThenTheUnitPriceShouldBe(decimal expectedUnitPrice)
         {
-            var result = GetRegistrationPrice();
-            Assert.That(result.UnitPrice, Is.EqualTo(expectedUnitPrice));
-        }
-
-        RegistrationPriceResult GetRegistrationPrice()
-        {
-            var eventDate = conferencesRepository.LoadAll().First(x => x.ConferenceName.Equals(ConferenceName)).EventDate;
-            UtcTime.Stop(eventDate.AddDays(-DaysUntilConference));
-            return conferencesController.RegistrationPrice(ConferenceName, NumberOfRegistrations, CouponCode).Data as RegistrationPriceResult;
+            ScenarioContext.Current.Pending();
         }
     }
 }
