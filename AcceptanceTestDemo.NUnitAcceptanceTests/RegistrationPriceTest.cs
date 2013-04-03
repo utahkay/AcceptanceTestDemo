5e703adbbc01e7693bbb0bd7bc9c951f675f062c
Kay Johansen <kay-johansen@pluralsight.com>
2013-04-03 13:23:14 -0600

    reformatted lines for readability on projector

--- AcceptanceTestDemo.NUnitAcceptanceTests/RegistrationPriceTest.cs
+++ AcceptanceTestDemo.NUnitAcceptanceTests/RegistrationPriceTest.cs
@@ -14,19 +14,20 @@ namespace AcceptanceTestDemo.NUnitAcceptanceTests
         readonly ConferencesController conferencesController = new ConferencesController();
         readonly ConferencesRepository conferencesRepository = new ConferencesRepository();
         DateTime EventDate;
-        const string ConferenceName = "Agile Austria";
 
         [SetUp]
         public void SetDateForNormalOnTimeRegistration()
         {
-            EventDate = conferencesRepository.LoadAll().First(x => x.ConferenceName.Equals(ConferenceName)).EventDate;
+            EventDate = conferencesRepository.LoadAll().First(x => x.ConferenceName.Equals("Agile Austria")).EventDate;
             UtcTime.Stop(EventDate.AddDays(-10));
         }
 
         [Test]
         public void SingleRegistration()
         {
-            var result = conferencesController.RegistrationPrice(ConferenceName, 1);
+            var result = conferencesController.RegistrationPrice(
+                conferenceName: "Agile Austria",
+                numRegistrations: 1);
             var value = result.Data as RegistrationPriceResult;
             Assert.That(value.TotalPrice, Is.EqualTo(500m));
         }
@@ -34,7 +35,9 @@ namespace AcceptanceTestDemo.NUnitAcceptanceTests
         [Test]
         public void GroupRegistration()
         {
-            var result = conferencesController.RegistrationPrice(ConferenceName, 5);
+            var result = conferencesController.RegistrationPrice(
+                conferenceName: "Agile Austria",
+                numRegistrations: 5);
             var value = result.Data as RegistrationPriceResult;
             Assert.That(value.UnitPrice, Is.EqualTo(450m));
             Assert.That(value.TotalPrice, Is.EqualTo(2250m));
@@ -43,7 +46,10 @@ namespace AcceptanceTestDemo.NUnitAcceptanceTests
         [Test]
         public void DiscountCode()
         {
-            var result = conferencesController.RegistrationPrice(ConferenceName, 1, "HALFOFF");
+            var result = conferencesController.RegistrationPrice(
+                conferenceName: "Agile Austria",
+                numRegistrations: 1,
+                couponCode: "HALFOFF");
             var value = result.Data as RegistrationPriceResult;
             Assert.That(value.TotalPrice, Is.EqualTo(250m));
         }
@@ -52,7 +58,9 @@ namespace AcceptanceTestDemo.NUnitAcceptanceTests
         public void LateRegistration()
         {
             UtcTime.Stop(EventDate.AddDays(-6));
-            var result = conferencesController.RegistrationPrice(ConferenceName, 1);
+            var result = conferencesController.RegistrationPrice(
+                conferenceName: "Agile Austria",
+                numRegistrations: 1);
             var value = result.Data as RegistrationPriceResult;
             Assert.That(value.TotalPrice, Is.EqualTo(700m));
         }
